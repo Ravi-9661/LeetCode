9c7a413daadfcb56c8dbd9630a4d13ed02bb316c
Raviraj <222743662+Ravi-9661@users.noreply.github.com>
2026-08-29 09:39:56 +0530

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/maximum-subarray.cpp
+++ divide-and-conquer/maximum-subarray.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
 
-        int currSum = nums[0];
+        int currSum = 0;
         int maxSum = nums[0];
 
-        for (int i = 1; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
             currSum +=nums[i];
             maxSum = max(maxSum, currSum);
             if(currSum<0) currSum=0;
